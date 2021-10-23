858ec8c90e41acc1e28cbca483aeb72a81c77d5a
Borik4 <87005120+Borik4@users.noreply.github.com>
2021-10-23 20:37:34 +0400

    Update client.py
    all

--- client.py
+++ client.py
@@ -5,19 +5,22 @@ from settings import *
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(IP_PORT)
+
+
 def listen_server():
     while True:
-        data = client.recv(2048)
-        print(data.decode('utf-8'))
-
+        data = client.recv(MAX)
+        print('                   ', data.decode('utf-8'))
 
 
 def send_server():
     lis_tread = Thread(target=listen_server)
     lis_tread.start()
+    # client.send(input('username: ').encode('utf-8'))
     while True:
 
-        client.send(input(':::').encode('utf-8'))
+        client.send(input('').encode('utf-8'))
+
 
 if __name__ == '__main__':
     send_server()
