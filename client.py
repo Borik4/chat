9e7f2300ff6875f2f814e5794867023ab967a9dc
Borik4 <87005120+Borik4@users.noreply.github.com>
2021-10-23 20:41:46 +0400

    Update client.py

--- client.py
+++ client.py
@@ -1,35 +1,26 @@
 import socket
-import threading
+from threading import Thread
 from settings import *
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(IP_PORT)
-server.listen(5)
-users = []
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(IP_PORT)
 
 
-def send_all(data, user, address):
-    for use in users:
-        if user != use:
-            use.send(f'{address} -> {data.decode()}'.encode())
-
-
-def listen_user(user, address):
-    print('listening')
+def listen_server():
     while True:
-        data = user.recv(MAX)
-        print('user send', data.decode('utf-8'))
-        send_all(data, user, address)
+        data = client.recv(MAX)
+        print('                   ', data.decode('utf-8'))
 
 
-def start():
+def send_server():
+    lis_tread = Thread(target=listen_server)
+    lis_tread.start()
+    # client.send(input('username: ').encode('utf-8'))
     while True:
-        user_socket, address = server.accept()
-        print(f'user: <{address[0]}> connected!!')
-        users.append(user_socket)
-        listen_accept_user = threading.Thread(target=listen_user, args=(user_socket, address))
-        listen_accept_user.start()
+
+        client.send(input('').encode('utf-8'))
 
 
 if __name__ == '__main__':
-    start()
+    send_server()
