6b529995f195499b26ba786e9646d119ed4f2949
Borik4 <87005120+Borik4@users.noreply.github.com>
2021-10-23 20:40:36 +0400

    Update server.py
    all

--- server.py
+++ server.py
@@ -7,29 +7,29 @@ server.bind(IP_PORT)
 server.listen(5)
 users = []
 
-def send_all(data):
-    for user in users:
-        print(user)
-        user.send(data)
+
+def send_all(data, user, address):
+    for use in users:
+        if user != use:
+            use.send(f'{address} -> {data.decode()}'.encode())
 
 
-def listen_user(user):
+def listen_user(user, address):
     print('listening')
     while True:
-        data = user.recv(2048)
-        print('user send', data)
-        send_all(data)
+        data = user.recv(MAX)
+        print('user send', data.decode('utf-8'))
+        send_all(data, user, address)
+
 
 def start():
     while True:
         user_socket, address = server.accept()
         print(f'user: <{address[0]}> connected!!')
         users.append(user_socket)
-        listen_accept_user = threading.Thread(target=listen_user, args=(user_socket,))
+        listen_accept_user = threading.Thread(target=listen_user, args=(user_socket, address))
         listen_accept_user.start()
 
 
 if __name__ == '__main__':
     start()
-
-
