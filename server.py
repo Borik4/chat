86c1dd2cdc65e5826f355de0fc52a607a16555fc
Borik4 <87005120+Borik4@users.noreply.github.com>
2021-10-24 20:23:16 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,37 @@
-import socket
 import threading
 from settings import *
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(IP_PORT)
-server.listen(5)
-users = []
+from main import Socket
 
 
-def send_all(data, user, address):
-    for use in users:
-        if user != use:
-            use.send(f'{address} -> {data.decode()}'.encode())
+class Server(Socket):
+    def __init__(self):
+        super(Server, self).__init__()
+        self.bind(IP_PORT)
+        self.listen(5)
+        print('Server is listening')
+        self.users = []
 
+    def send_data(self, data, user, address):
+        for use in self.users:
+            if user != use:
+                use.send(f'{address} -> {data.decode()}'.encode())
 
-def listen_user(user, address):
-    print('listening')
-    while True:
-        data = user.recv(MAX)
-        print('user send', data.decode('utf-8'))
-        send_all(data, user, address)
+    def listen_socket(self, user, address):
+        print('listening')
+        while True:
+            data = user.recv(MAX)
+            print('user send', data.decode('utf-8'))
+            self.send_data(data, user, address)
 
-
-def start():
-    while True:
-        user_socket, address = server.accept()
-        print(f'user: <{address[0]}> connected!!')
-        users.append(user_socket)
-        listen_accept_user = threading.Thread(target=listen_user, args=(user_socket, address))
-        listen_accept_user.start()
+    def start(self):
+        while True:
+            user_socket, address = self.accept()
+            print(f'user: <{address[0]}> connected!!')
+            self.users.append(user_socket)
+            listen_accept_user = threading.Thread(target=self.listen_socket, args=(user_socket, address))
+            listen_accept_user.start()
 
 
 if __name__ == '__main__':
-    start()
+    al = Server()
+    al.start()
